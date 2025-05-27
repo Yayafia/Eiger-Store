bdd46fef0dd1491a23463cfea2a7db1ae8687c3f
Yayafia <ardyayaya1303@gmail.com>
2025-05-27 21:40:40 +0700

    Tugas Mingguan Prak Oop Bab 9
    Tugas Mingguan Prak Oop Bab 9

---
+++ TugasPrakOopBab9/src/TugasPrakOop/BAB9/EigerCasualStore.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPrakOop.BAB9;
+
+/**
+ *
+ * @author USER
+ */
+public class EigerCasualStore extends EigerStore implements Promo {
+    private String jenisKelamin;
+    private String warna;
+    private String tambahanInfo;
+
+    public EigerCasualStore(String namaToko, String lokasi, String jenisKelamin, String warna, String tambahanInfo) {
+        super(namaToko, lokasi);
+        this.jenisKelamin = jenisKelamin;
+        this.warna = warna;
+        this.tambahanInfo = tambahanInfo;
+    }
+
+    @Override
+    public String getJenisStore() {
+        return "Casual Store";
+    }
+
+    @Override
+    public String tampilkanInfoToko() {
+        return "=== EIGER CASUAL STORE ===\n" +
+               "Nama Toko : " + namaToko + "\n" +
+               "Lokasi    : " + lokasi + "\n" +
+               "JK        : " + jenisKelamin + "\n" +
+               "Warna     : " + warna + "\n" +
+               "Tambahan  : " + tambahanInfo + "\n" +
+               "Promo     : " + infoPromo() + "\n" +
+               "Alat Pendakian: Tidak\n";
+    }
+
+    public String infoPromo() {
+        return "Beli 2 kaos gratis 1 topi!";
+    }
+  
+}
