6a980472dcb18f75768db11b2f70b322a552cac7
Yayafia <ardyayaya1303@gmail.com>
2025-05-29 23:27:59 +0700

    Tugas Prak Mingguan Bab 10
    Tugas Prak Mingguan Bab 10

---
+++ TugasPrakOopBab10/src/TugasPrakOop/BAB_10/EigerAdventureStore.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPrakOop.BAB_10;
+
+/**
+ *
+ * @author USER
+ */
+public class EigerAdventureStore extends EigerStore implements Promo {
+    private String aktivitas;
+    private String musim;
+    private String tambahanInfo;
+
+    public EigerAdventureStore(String namaToko, String lokasi, String aktivitas, String musim, String tambahanInfo) {
+        super(namaToko, lokasi);
+        this.aktivitas = aktivitas;
+        this.musim = musim;
+        this.tambahanInfo = tambahanInfo;
+    }
+
+    @Override
+    public String getJenisStore() {
+        return "Adventure Store";
+    }
+
+    @Override
+    public String tampilkanInfoToko() {
+        return "=== EIGER ADVENTURE STORE ===\n" +
+               "Nama Toko : " + namaToko + "\n" +
+               "Lokasi    : " + lokasi + "\n" +
+               "Aktivitas : " + aktivitas + "\n" +
+               "Musim     : " + musim + "\n" +
+               "Tambahan  : " + tambahanInfo + "\n" +
+               "Promo     : " + infoPromo() + "\n" +
+               "Alat Pendakian: Iya\n";
+    }
+
+    public String infoPromo() {
+        return "Diskon 10% untuk perlengkapan hiking!";
+    } 
+}
---
+++ TugasPrakOopBab10/src/TugasPrakOop/BAB_10/Promo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package TugasPrakOop.BAB_10;
+
+/**
+ *
+ * @author USER
+ */
+public interface Promo {
+     String infoPromo(); // method abstrak
+}
